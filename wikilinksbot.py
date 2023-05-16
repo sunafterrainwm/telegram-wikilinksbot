52e42ef815ee53631445b79c99ba70ae5a5f6e06
Peace1-zhwiki <staratzhwiki@gmail.com>
2023-05-16 23:48:19 +0000

    Fix bug

--- wikilinksbot.py
+++ wikilinksbot.py
@@ -14,16 +14,14 @@ regex = re.compile(r"(\[\[.+?\]\]|(?<!\{)\{\{(?!\{).+?\}\}|(?<![\w%#.\-])(?<![A-
 # Load the group settings file once for every time the script is run.
 # If any settings change, global_conf will be set again.
 global_conf = {}
-with open("group_settings.json", "r") as settings:
+with open("/data/project/zhwiki-teleirc/telegram-wikilinksbot/group_settings.json", "r") as settings:
     global_conf = json.load(settings)
 
 messages = {
-    "start-group": ("🤖您好！我是連結機器人，當訊息中有提及任何[[維基連結]]的時
-候，就會提供連結。"
+    "start-group": ("🤖您好！我是連結機器人，當訊息中有提及任何[[維基連結]]的時候，就會提供連結。"
     "您可以<a href='https://t.me/linky_zh_bot'>私訊我</a>來測試各項功能。"
     "您可以使用 <code>/delete</code> 指令回覆我的任何訊息以刪除之。祝 編安！"),
-    "start-private": ("🤖您好！我是連結機器人，當訊息中有提及任何[[維基連結]]的
-時候，就會提供連結。您可以在此測試以下這些功能如何運作：\n\n🤖"
+    "start-private": ("🤖您好！我是連結機器人，當訊息中有提及任何[[維基連結]]的時候，就會提供連結。您可以在此測試以下這些功能如何運作：\n\n🤖"
             "/setwiki – 更改連結路徑\n"
             "/setlang – Change which language to use for Wikidata labels\n"
             "/toggle – Turn the link types on or off\n"
